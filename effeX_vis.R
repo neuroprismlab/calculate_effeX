a964b92d26c40aa71d399097c047b944497f0487
Hallee Shearer <halleeshearer@outlook.com>
2024-03-19 17:38:14 -0400

    updates

--- effeX_vis.R
+++ effeX_vis.R
@@ -2,8 +2,8 @@
 # TODO: check why the confidence interval for some studies (e.g. ukb fc r rest memory) doesn't include d at the ends
 # TODO: shade the intervals red if they include zero (i.e. not significant)
 
-plot_sim_ci <- function(v_d_clean, i) {
-  data <- v_d_clean[[i]]
+plot_sim_ci <- function(d_clean, i) {
+  data <- d_clean[[i]]
   # remove na
   na_idx <- is.na(data$d) | is.na(data$sim_ci_lb) | is.na(data$sim_ci_ub)
   data$d <- data$d[!na_idx]
@@ -15,10 +15,16 @@ plot_sim_ci <- function(v_d_clean, i) {
   sorted_lower_bounds <- data$sim_ci_lb[sorted_indices]
   
   plot(sorted_d, type = "l", ylim = c(min(sorted_lower_bounds, na.rm = TRUE), max(sorted_upper_bounds, na.rm = TRUE)),
-       main = names(v_d_clean[i]), xlab = "Edges/Voxels", ylab = "Cohen's d")
+       main = names(d_clean[i]), xlab = "Edges/Voxels", ylab = "Cohen's d")
   
   polygon(c(1:length(sorted_d), rev(1:length(sorted_d))), 
           c(sorted_upper_bounds, rev(sorted_lower_bounds)), 
           col = rgb(0.5, 0.5, 0.5, alpha = 0.3), border = NA)
+}
+
+# plot all studies 
+par(mfrow=c(((length(d_clean) %/% 4) + 1), 4))  # Set up multiple plots side by side
+for (i in 1:length(d_clean)) {
+  plot_sim_ci(d_clean, i)
 }
 
