77f876befc0c86b0972227c9972bc7b2576922d5
sneuroble <s.noble@northeastern.edu>
2024-09-16 14:29:33 -0400

    Slight speedup to averaging; accounted for 0's in atlas

--- group_level/helper_scripts/atlas_tools/average_within_3d_atlas.m
+++ group_level/helper_scripts/atlas_tools/average_within_3d_atlas.m
@@ -1,16 +1,14 @@
-function data_avg = average_within_3d_atlas(data,atlas,return_3d)
-% average 3D map within predefined regions of atlas
-% input:    atlas: must be image from load_nii
-%           data: must be same space and orientation as atlas (this script will only check dimensions)
-%           return_3d: return 3D image containing average value for each voxel within an area; otherwise will return single value per area
-% output: 3D or 2D matrix representing average within each area (see "return_3d"
+function data_avg = average_within_3d_atlas(data,atlas,return_structured)
+% average map within predefined regions of atlas
 %
-% personal atlases:
-%   node-level: /Users/steph/Lab/Misc/Software/data/bioimagesuite/images/shenetal_neuroimage2013/shen_1mm_268_parcellation.nii.gz
-%   community-level: /Users/steph/Lab/Misc/Software/data/bioimagesuite/images/shenetal_neuroimage2013/shen_1mm_268_parcellation__in_subnetworks.nii.gz
+% input:    atlas: n x m integer matrix
+%           data: n x m numeric matrix (if loaded from nii, ensure this is the same space and orientation as atlas (this script will only check dimensions))
+%           return_structured: return matrix structured in the same space as the original atlas (i.e., each entry contains average value for its group); otherwise return single value per group
+%
+% output: 1 x n_groups or n x m matrix of average within each area
+
 
 % check stuff
-
 if ~isequal(size(data) , size(atlas))
     error(['Sizes don''t match. Data size is ',sprintf('%d ',size(data)),', atlas size is ',sprintf('%d ',size(atlas))]);
 end
@@ -19,26 +17,25 @@ if min(atlas(:)) < 0
     warning('Negative entries found in atlas. Unusual - you may want to check this.')
 end
 
-% summarize
+% filter out zero values
+non_zero_indices = atlas(:) ~= 0;
+filtered_atlas = atlas(non_zero_indices);
+filtered_data = data(non_zero_indices);
 
-unique_atlas_regions=unique(atlas(:));
-data_avg=zeros(size(unique_atlas_regions));
+% calculate mean
+data_avg = accumarray(filtered_atlas, filtered_data, [], @mean);
 
-for i=unique_atlas_regions
-    data_avg(i)=mean(data(atlas==i));
+% optional: structure data
+if return_structured
+    data_avg_structured = zeros(size(atlas)); % Initialize with zeros
+    unique_atlas_regions = unique(filtered_atlas);
+    % Create a mask for all regions at once
+    [~, loc] = ismember(atlas, unique_atlas_regions);
+    % Assign values in a vectorized manner, ignoring zeros
+    non_zero_mask = atlas ~= 0;
+    data_avg_structured(non_zero_mask) = data_avg(loc(non_zero_mask));
+    data_avg = data_avg_structured;
 end
 
-if return_3d
-    data_avg_3d=atlas;
-    for i=unique_atlas_regions
-        data_avg_3d(atlas==i)=data_avg(i);
-    end
-    data_avg=data_avg_3d;
-end
-
-%for i = min(atlas(atlas~=0)):max(atlas(:))
-%    ind=find(atlas==i);
-%    data_atlas(ind)=mean(data(ind));
-%end
 
 
