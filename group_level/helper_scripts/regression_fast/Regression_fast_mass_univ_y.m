a3af68aae3cbba3d4f94f9336d609996519d8c89
sneuroble <s.noble@northeastern.edu>
2024-10-08 08:59:33 -0400

    minor speedups

--- group_level/helper_scripts/regression_fast/Regression_fast_mass_univ_y.m
+++ group_level/helper_scripts/regression_fast/Regression_fast_mass_univ_y.m
@@ -1,4 +1,4 @@
-function[Val] = Regression_fast_mass_univ_y(x,y,Pval)
+function[T,P] = Regression_fast_mass_univ_y(x,y)
 
 % INPUTS
 % x:    nxp regressor matrix of n observations (rows) across
@@ -14,27 +14,18 @@ function[Val] = Regression_fast_mass_univ_y(x,y,Pval)
 % USE
 % Val = Regression_fast(x,y)
 
-% Lightly edited by Stephanie Noble (Aug 2024) to return mass univariate estimates for multiple y simultaneously, and also to return t-stats
-
-if nargin == 2
-    Pval = false;
-end
+% Lightly edited by Stephanie Noble to return mass univariate estimates for multiple y simultaneously, to return t-stats, and small speedups (matrix left division, no logic, etc)
 
 B   = (x'*x)\x'*y;
-Val = B;
 
-if Pval    
     y_fit = x*B;
     df  = -diff(size(x));
 
     s2 = sum((y-y_fit).^2)/df;
     %s   = (sum((y-y_fit).^2)/df)^0.5;
-    se  = (diag(inv(x'*x))*s2).^0.5;
+    se = sqrt(diag((x' * x) \ eye(size(x, 2))) * s2);
     T   = B./se;
-    
-    P   = (T>=0).*(1 - tcdf(T,df))*2 + (T<0).*(tcdf(T,df))*2;
-    
-    Val = cat(3,B,P,T);
-end
+   
+    P = 2 * (1 - tcdf(abs(T), df));
 
 end
