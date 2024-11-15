cf80ff7c5e1d2b9de525c21885db8cb725196059
sneuroble <s.noble@northeastern.edu>
2024-11-15 15:29:14 -0500

    Make separate function r->t so can access from main group level script

--- group_level/helper_scripts/partial_and_semipartial_corr/partial_and_semipartial_corr.m
+++ group_level/helper_scripts/partial_and_semipartial_corr/partial_and_semipartial_corr.m
@@ -1,4 +1,5 @@
 function [r_part, t_part, p_part, r_semipart, t_semipart, p_semipart] = partial_and_semipartial_corr(y,x,z,n)
+
     % y is outcome, x is predictor, and z is confound, n is sample size
 
     r_pairwise = corr_generalized(y, x, z);
@@ -38,15 +39,4 @@ function [r_part, t_part, p_part] = r_partial_from_pairwise(r_yx, r_yz, r_xz)
     % https://www.listendata.com/2017/03/partial-correlation.html
     r_part = (r_yx - r_yz .* r_xz) ./ ((1 - r_xz .^2) * (1 - r_yz .^2)) .^0.5;
 end
-
-function [t,p] = r_to_test_stats(r, n, n_confounds, n_groups)
-    % for test, following Kim 2015 (creator of ppcor) - validated in https://www.ncbi.nlm.nih.gov/pmc/articles/PMC9230004/ 
-    % two-sided
-
-    % TODO: test whether same as directly from r -> p
-    t = r .* ((n - 2 - n_confounds) ./ (1 - r .^2)) .^0.5;
-    p = 2 * (1 - tcdf(abs(t), (n - n_groups - n_confounds) )); % note: tcdf is vectorized
-
-end
-
 
